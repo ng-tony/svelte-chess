3ee3b2d8f0c3c2267343dba6bd2080597e66a0d2
tony <tonyn.20500@gmail.com>
2022-01-29 23:31:58 -0500

    Touch moving a piece!

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+  import Piece from "./Piece.svelte";
+
   export let board = Array.from(Array(64), (_, i) => i);
   let grid;
+  let squares = [];
+  let activeEvent = "";
+  let endSquare = 0;
 
   const dark = "bg-red-100";
   const light = "bg-blue-100";
@@ -8,15 +13,71 @@
   // i 8 / 0 | i + 2 %
   const squareColor = (square: number) =>
     (square + ((square / 8) | 0)) % 2 ? dark : light;
+
+  let originalX = "";
+  let originalY = "";
+  let movingObject;
+
+  function handleTouchStart(e) {
+    movingObject = e.target;
+    originalX = e.target.offsetLeft - 10 + "px";
+    originalY = e.target.offsetTop - 10 + "px";
+    activeEvent = "start";
+  }
+
+  function handleTouchMove(e: TouchEvent & { target: HTMLDivElement }) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.touches.length === 0) return;
+    let { pageX, pageY } = e.touches[0];
+    e.target.style.position = "absolute";
+    e.target.style.left = `${pageX - movingObject.offsetWidth / 2}px`;
+    e.target.style.top = `${pageY - movingObject.offsetHeight / 2}px`;
+    activeEvent = "move";
+  }
+
+  function handleTouchEnd(e) {
+    e.preventDefault();
+
+    if (activeEvent === "move") {
+      const { pageX, pageY } = e.changedTouches[0];
+
+      endSquare = getSquare(pageX, pageY);
+
+      if (endSquare !== null) {
+        squares[endSquare].children = e.target;
+      }
+    }
+  }
+
+  function getSquare(x, y) {
+    const { offsetLeft, offsetTop, offsetWidth, offsetHeight } = grid;
+    const [relX, relY] = [x - offsetLeft, y - offsetTop];
+    console.log({ relX, relY });
+    if (relX < 0 || relX > offsetWidth) return null;
+    if (relY < 0 || relY > offsetHeight) return null;
+
+    const squareLen = offsetHeight / 8;
+    return (((relX / squareLen) | 0) % 8) + 8 * (((relY / squareLen) | 0) % 8);
+  }
 </script>
 
+<Piece
+  on:touchstart={handleTouchStart}
+  on:touchmove={handleTouchMove}
+  on:touchend={handleTouchEnd}
+/>
+
 <grid class="board grid grid-cols-8" bind:this={grid}>
-  {#each board as square}
-    <grid-item class="flex pb-full {squareColor(square)} w-full"
-      >{square}</grid-item
+  {#each board as square, i}
+    <grid-item
+      class="flex pb-full {squareColor(square)} w-full"
+      bind:this={squares[i]}>{square}</grid-item
     >
   {/each}
 </grid>
+
+{endSquare}
 
 <style>
   grid-item::before {
---
+++ src/lib/Piece.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div draggable on:touchstart on:touchmove on:touchend />
+
+<style>
+  div {
+    position: absolute;
+    display: block;
+    background: url("/src/assets/img/bb.svg");
+    height: 40px;
+    width: 40px;
+    flex-shrink: 0;
+  }
+
+  div::before {
+    height: 40px;
+  }
+</style>
